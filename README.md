0bdd86677f1b67134153691676034e4b290f3bed
Immanuel Irwin <noelimmanuel1998@gmail.com>
2021-04-04 20:17:14 +0530

    Initial setup for proxy server with available services

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proxy server for backend services
+
+Node server will act as proxy server for other backend services
---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const SERVICE_VS_PORT = {};
+SERVICE_VS_PORT['auth'] = 6000;
+SERVICE_VS_PORT['core'] = 6001;
+SERVICE_VS_PORT['catalogue'] = 6002;
+SERVICE_VS_PORT['pos'] = 6003;
+SERVICE_VS_PORT['ecom'] = 6004;
+
+//Auth Service
+app.use(
+  '/auth',
+  createProxyMiddleware({
+    target: `http://localhost:${SERVICE_VS_PORT.auth}`,
+    changeOrigin: false,
+    pathRewrite: {
+      '^/auth': '/', // rewrite path
+    },
+  })
+);
+
+//Core Service
+app.use(
+  '/core',
+  createProxyMiddleware({
+    target: `http://localhost:${SERVICE_VS_PORT.core}`,
+    changeOrigin: false,
+    pathRewrite: {
+      '^/core': '/', // rewrite path
+    },
+  })
+);
+
+//Catalogue Service
+app.use(
+  '/catalogue',
+  createProxyMiddleware({
+    target: `http://localhost:${SERVICE_VS_PORT.catalogue}`,
+    changeOrigin: false,
+    pathRewrite: {
+      '^/catalogue': '/', // rewrite path
+    },
+  })
+);
+
+//POS Service
+app.use(
+  '/pos',
+  createProxyMiddleware({
+    target: `http://localhost:${SERVICE_VS_PORT.pos}`,
+    changeOrigin: false,
+    pathRewrite: {
+      '^/pos': '/', // rewrite path
+    },
+  })
+);
+
+//Ecommerce Service
+app.use(
+  '/ecom',
+  createProxyMiddleware({
+    target: `http://localhost:${SERVICE_VS_PORT.ecom}`,
+    changeOrigin: false,
+    pathRewrite: {
+      '^/ecom': '/', // rewrite path
+    },
+  })
+);
+
+const SERVER_PROXY_PORT = 6005;
+app.listen(SERVER_PROXY_PORT, () =>
+  console.log(`Proxy server started on port ${SERVER_PROXY_PORT}`)
+);
